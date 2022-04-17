fdce6b33207d118e3a017b071061ebce6a323129
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-04-17 16:44:56 +0600

    login, reset password, email veryficaiton done

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,25 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Sociallogin from "../socialLogin/SocialLogin";
-
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../firebase/Firebase.int";
+import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const [sendPasswordResetEmail, sending, error2] =
+    useSendPasswordResetEmail(auth);
+  const formHandler = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(email, password);
+    setEmail("");
+    setPassword("");
+  };
+
+  const resetPassword = async () => {
+    await sendPasswordResetEmail(email);
+    notify();
+    setEmail("");
+  };
+
+  const notify = () => toast("Please Check Your Email !");
+
   return (
     <div className="auth-container">
       <div>
         <Sociallogin />
         <div className="form-container">
           <h3>login</h3>
-          <form action="">
-            <input type="email" placeholder="enter your email" required />
-            <input type="password" placeholder="enter your password" required />
+          <form onSubmit={formHandler} action="">
+            <input
+              type="email"
+              placeholder="enter your email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="enter your password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <p
+              className="error"
+              style={{
+                textAlign: "initial",
+                color: "red",
+                textAlignLast: "initial",
+              }}
+            >
+              {error?.message}
+            </p>
             <input className="btn" type="submit" value="login" />
           </form>
           <p>
             new to tony robbins?{" "}
-            <Link className="link" to="/login">
+            <Link className="link" to="/registar">
               create account
             </Link>
           </p>
+          <span
+            style={{
+              color: "#000",
+              textDecoration: "underLine",
+              cursor: "pointer",
+              fontSize: "1.7rem",
+              marginTop: "1rem",
+            }}
+            onClick={resetPassword}
+          >
+            forgot password
+          </span>
+          <ToastContainer />
         </div>
       </div>
     </div>
--- src/components/registar/Registar.js
+++ src/components/registar/Registar.js
@@ -1,18 +1,57 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Sociallogin from "../socialLogin/SocialLogin";
 import "./Registar.css";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../firebase/Firebase.int";
 
 const Registar = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+  const formHandler = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(email, password);
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
   return (
     <div className="auth-container">
       <div>
         <Sociallogin />
         <div className="form-container">
           <h3>registar</h3>
-          <form action="">
-            <input type="text" placeholder="enter your name" required />
-            <input type="email" placeholder="enter your email" required />
-            <input type="password" placeholder="enter your password" required />
+          <form onSubmit={formHandler} action="">
+            <input
+              type="text"
+              placeholder="enter your name"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="enter your email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="enter your password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <p
+              className="error"
+              style={{ textAlign: "initial", color: "red", margin: 0 }}
+            >
+              {error?.message}
+            </p>
             <input className="btn" type="submit" value="registar" />
           </form>
           <p>
