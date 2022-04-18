b36108eb3e76ff69cbf425acc0c3af90c7da9d4a
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-04-18 14:44:51 +0600

    about and blogs page done

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,7 +9,17 @@ const About = () => {
           <img src="images/majedul.jpg" alt="" />
         </div>
         <h3>majedul islam</h3>
-        <p></p>
+        <p>
+          Hi, I am Majedul Islam. In the next five to six months, I will have to
+          get a job as a front-end developer in a company. I will do everything
+          I need to do for it. Actually, I want to be a good quality full-stack
+          web developer and I know I still have a long way to go to be a
+          full-stack web developer. And in order to go this long way I need to
+          learn as well as earn some money.Which will give me the strength to be
+          more diligent and more interested in learning. Hopefully, by the grace
+          of Allah, I will be able to fulfill my goals and dreams through my own
+          efforts and programming-hero.
+        </p>
       </div>
       <Footer />
     </>
--- src/components/blogs/Blogs.js
+++ src/components/blogs/Blogs.js
@@ -1,11 +1,53 @@
 import React from "react";
 import Footer from "../footer/Footer";
-
+import "./Blogs.css";
 const Contact = () => {
   return (
     <>
-      <div>
-        <h1>this is blogs page</h1>
+      <div className="blogs">
+        <h3>what is difference between authorization and authentication?</h3>
+        <p>
+          If I simply tell authentication is the process of verifying someone
+          who is on the other hand authorization is the process of verifying
+          what is user has access to. Authentication is the first step of a good
+          identity and access management process.Authorization always takes
+          place after authentication.
+        </p>
+        <h3>
+          why are using firebase? what oter option do you have to implement
+          authentication?
+        </h3>
+        <p>
+          Google firebase provides many different kinds of services like
+          authentication, data base, hosting, realtime database, storage etc.
+          And it is free and easy to use. So we are using firebase. There are
+          different types of option to implement authentication.
+        </p>
+        <ul
+          style={{
+            margin: "1rem 2rem",
+            fontSize: "1.7rem",
+            lineHeight: "3rem",
+          }}
+        >
+          <li>
+            Password-based authentication. Passwords are the most common methods
+            of authentication
+          </li>
+
+          <li>Multi-factor authentication</li>
+          <li>Certificate-based authentication</li>
+          <li>Biometric authentication</li>
+          <li>Token-based authentication.</li>
+        </ul>
+
+        <h3> what other services firebase provide other than authenction?</h3>
+        <p>
+          Many other services firebase provide other than authenction. Google
+          firebase provides many different kinds of services without authenction
+          like data base, hosting, realtime database, storage, function, machine
+          learnign etc.
+        </p>
       </div>
       <Footer />
     </>
